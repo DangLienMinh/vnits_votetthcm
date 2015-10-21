f832757fd0f5f527d130477da0a7b1977970a225
DangLienMinh <uit11520232@gmail.com>
2015-10-21 15:10:12 +0700

    change giao dien final

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require ckeditor/init
-//= require turbolinks
-//= require_tree ./fileupload
+//= require bootstrap
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   
   get 'welcome/index'
   get 'welcome/newsdetail'
+   get 'welcome/newsindex'
   get 'welcome/peopledetail'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
